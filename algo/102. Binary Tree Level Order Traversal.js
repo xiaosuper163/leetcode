2118e8d62f5b135bce94755d8f2efa01da0de4fc
xiaosuper163 <yabing@sfu.ca>
2019-01-20 17:53:29 -0800

    add solution code of question 102

---
+++ algo/102. Binary Tree Level Order Traversal.js
@@ -0,0 +1,41 @@
+// @9 BFS
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+    if (root == null) {
+        return [];
+    }
+    
+    var res = [];
+    var queue = [root];
+    while (queue.length != 0) {
+        var levelRes = [];
+        var levelSize = queue.length;
+
+        for (var i=0; i<levelSize; i++) {
+            let currNode = queue.shift();
+            levelRes.push(currNode.val);
+            if (currNode.left) {
+                queue.push(currNode.left);
+            }
+
+            if (currNode.right) {
+                queue.push(currNode.right);
+            }
+        }
+
+        res.push(levelRes);
+    }
+    
+    return res;
+};
