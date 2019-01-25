20c385d2d6c94426ef93361dbacea2da59c1d382
xiaosuper163 <yabing@sfu.ca>
2019-01-24 18:29:01 -0800

    add solution code of question 78

---
+++ algo/78. Subsets.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+    
+    var helper = function(res, collection, i) {
+        // out of bound
+        var collection = Array.from(collection);       
+        if (i > nums.length) {
+            return;
+        }
+        res.push(collection);
+        
+        for (var index = i; index < nums.length; index++) {
+            collection.push(nums[index]);
+            helper(res, collection, index+1);
+            collection.pop();
+        }
+        
+    }
+    
+    var res = [];
+    if (nums.length == 0) {
+        return [];
+    }
+    nums.sort();
+    helper(res, [], 0);
+        
+    return res;
+};
