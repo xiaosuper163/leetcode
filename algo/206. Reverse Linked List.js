6105bb911c2e35a3ec3157334db3b96dd8a34223
xiaosuper163 <yabing@sfu.ca>
2019-01-27 17:22:28 -0800

    add solution code of question 206

---
+++ algo/206. Reverse Linked List.js
@@ -0,0 +1,26 @@
+// @9 linked list
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    var prev = null;
+    var curr = head;
+    
+    while(curr !== null) {
+        var temp = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = temp;
+    }
+    
+    return prev;
+};
