587e53b6d1d2b445d4efdc24af8e5cb0f98012b1
xiaosuper163 <yabing@sfu.ca>
2019-02-03 21:24:07 -0800

    add O(n) solution code of two sum question

--- algo/1. Two Sum.js
+++ algo/1. Two Sum.js
@@ -1,3 +1,23 @@
+// @9 Two Sum
+// O(n) time complexity if using hashmap
+
+/*
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        nums_dict = {}
+        for i, num in enumerate(nums):
+            nums_dict[num] = i
+        for i, num in enumerate(nums):
+            if (target-num) in nums_dict.keys():
+                if i != nums_dict[target-num]:
+                    return [i, nums_dict[target-num]]
+*/
+
 /**
  * @param {number[]} nums
  * @param {number} target
